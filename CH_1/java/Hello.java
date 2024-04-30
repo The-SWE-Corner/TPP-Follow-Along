7e1948723b165cfd838b7e243380592ca7b4b60b
Sopuruchi <168391420+The-SWE-Corner@users.noreply.github.com>
2024-04-30 20:46:52 +0000

    Chapter 1 java codebase

---
+++ CH_1/java/Hello.java
@@ -0,0 +1,18 @@
+// public class Main{
+//     static void main(){
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+public class Hello {
+    static void myMethod() {
+      System.out.println("I just got executed!");
+    }
+  
+    public static void main(String[] args) {
+      myMethod();
+      myMethod();
+      myMethod();
+    }
+  }
